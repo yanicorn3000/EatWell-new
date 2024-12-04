412831b4af7842a7492a3ec028f7c1fb1b5c54fa
yanicorn3000 <yanadziabiola@gmail.com>
2024-12-04 17:55:20 +0100

    vite config fixes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: config.mode === "production" ? "/eatwell" : "/",
+  base: config.mode === "production" ? "/EatWell-new" : "/",
   root: "src",
   server: {
     port: 3000,
